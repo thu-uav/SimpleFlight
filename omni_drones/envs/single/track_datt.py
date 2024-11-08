eb6682235fe340fc4a0d0ad2f6c5d8617764c4e1
jiayu-ch15 <jiayu-ch15@mails.tsinghua.edu.cn>
2024-11-08 17:30:27 +0800

    update

--- omni_drones/envs/single/track_datt.py
+++ omni_drones/envs/single/track_datt.py
@@ -142,8 +142,8 @@ class Track_datt(IsaacEnv):
         else:
             self.ref = RandomZigzag(num_trajs=self.num_envs,
                                     max_D=[1.0, 1.0, 0.0],
-                                    min_dt=1.5,
-                                    max_dt=3.0,
+                                    min_dt=1.0,
+                                    max_dt=1.5,
                                     diff_axis=True,
                                     origin=self.origin,
                                     device=self.device)
