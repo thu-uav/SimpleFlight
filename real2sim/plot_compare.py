5364a473da7c8329273bb474000fc665ec0562b3
jiayu-ch15 <jiayu-ch15@mails.tsinghua.edu.cn>
2024-11-10 22:48:57 +0800

    update

--- real2sim/plot_compare.py
+++ real2sim/plot_compare.py
@@ -5,7 +5,7 @@ import torch
 # data = [(rpos, linear_velocity, rotation_matrix)]
 future_traj_steps = 4
 rpos_len = future_traj_steps * 3
-data = torch.load('/home/jiayu/OmniDrones/real2sim/slow_ab/data_11_3/obs_norm/sim_state.pt')
+data = torch.load('/home/jiayu/OmniDrones/real2sim/DATT/scale2_5_smooth5/sim_state.pt')
 data = torch.stack(data)
 # sim
 rpos = data[:-1, 0, :rpos_len].to('cpu').numpy()
@@ -16,39 +16,44 @@ up = data[:-1, 0, rpos_len + 9:rpos_len + 12].to('cpu').numpy()
 
 # real
 trajectory_scale = 'slow'
+obs_norm = False
 if trajectory_scale == 'slow':
     vel_range = 0.5
 elif trajectory_scale == 'normal':
     vel_range = 2.0
 else:
     vel_range = 3.0
-real_data = torch.load('/home/jiayu/OmniDrones/real2sim/slow_ab/data_11_3/obs_norm/real_state.pt')
+real_data = torch.load('/home/jiayu/OmniDrones/real2sim/DATT/scale2_5_smooth5/real_state.pt')
 real_obs = []
 for frame in real_data:
     real_obs.append(frame['agents', 'observation'][0,0])
 real_obs = torch.stack(real_obs)
 real_rpos = real_obs[..., :rpos_len].to('cpu').numpy()
-real_linear_vel = np.clip(real_obs[..., rpos_len:rpos_len + 3].to('cpu').numpy(), -0.5 / vel_range, 0.5 / vel_range)
+if obs_norm:
+    real_linear_vel = np.clip(real_obs[..., rpos_len:rpos_len + 3].to('cpu').numpy(), -0.5 / vel_range, 0.5 / vel_range)
+else:
+    real_linear_vel = np.clip(real_obs[..., rpos_len:rpos_len + 3].to('cpu').numpy(), -3.0, 3.0)
 real_heading = real_obs[..., rpos_len + 3:rpos_len + 6].to('cpu').numpy()
 real_lateral = real_obs[..., rpos_len + 6:rpos_len + 9].to('cpu').numpy()
 real_up = real_obs[..., rpos_len + 9:rpos_len + 12].to('cpu').numpy()
 
 time_steps = np.arange(len(rpos))
 real_time_steps = np.arange(len(real_rpos))
+show_len = 180
 
 fig, axs = plt.subplots(15, 1, figsize=(8, 15))
 
 # sim rpos
-axs[0].plot(real_time_steps, real_rpos[:, 0], label='real rpos x')
-axs[0].plot(time_steps, rpos[:, 0], label='sim rpos x')
+axs[0].plot(real_time_steps[:show_len], real_rpos[:show_len, 0], label='real rpos x')
+axs[0].plot(time_steps[:show_len], rpos[:show_len, 0], label='sim rpos x')
 axs[0].legend()
 
-axs[1].plot(real_time_steps, real_rpos[:, 1], label='real rpos y')
-axs[1].plot(time_steps, rpos[:, 1], label='sim rpos y')
+axs[1].plot(real_time_steps[:show_len], real_rpos[:show_len, 1], label='real rpos y')
+axs[1].plot(time_steps[:show_len], rpos[:show_len, 1], label='sim rpos y')
 axs[1].legend()
 
-axs[2].plot(real_time_steps, real_rpos[:, 2], label='real rpos z')
-axs[2].plot(time_steps, rpos[:, 2], label='sim rpos z')
+axs[2].plot(real_time_steps[:show_len], real_rpos[:show_len, 2], label='real rpos z')
+axs[2].plot(time_steps[:show_len], rpos[:show_len, 2], label='sim rpos z')
 axs[2].legend()
 
 # # sim quat
@@ -70,55 +75,55 @@ axs[2].legend()
 # axs[6].legend()
 
 # linear v
-axs[3].plot(real_time_steps, real_linear_vel[:, 0], label='real vx')
-axs[3].plot(time_steps, linear_vel[:, 0], label='sim vx')
+axs[3].plot(real_time_steps[:show_len], real_linear_vel[:show_len, 0], label='real vx')
+axs[3].plot(time_steps[:show_len], linear_vel[:show_len, 0], label='sim vx')
 axs[3].legend()
 
-axs[4].plot(real_time_steps, real_linear_vel[:, 1], label='real vy')
-axs[4].plot(time_steps, linear_vel[:, 1], label='sim vy')
+axs[4].plot(real_time_steps[:show_len], real_linear_vel[:show_len, 1], label='real vy')
+axs[4].plot(time_steps[:show_len], linear_vel[:show_len, 1], label='sim vy')
 axs[4].legend()
 
-axs[5].plot(real_time_steps, real_linear_vel[:, 2], label='real vz')
-axs[5].plot(time_steps, linear_vel[:, 2], label='sim vz')
+axs[5].plot(real_time_steps[:show_len], real_linear_vel[:show_len, 2], label='real vz')
+axs[5].plot(time_steps[:show_len], linear_vel[:show_len, 2], label='sim vz')
 axs[5].legend()
 
 # heading
-axs[6].plot(real_time_steps, real_heading[:, 0], label='real heading x')
-axs[6].plot(time_steps, heading[:, 0], label='sim heading x')
+axs[6].plot(real_time_steps[:show_len], real_heading[:show_len, 0], label='real heading x')
+axs[6].plot(time_steps[:show_len], heading[:show_len, 0], label='sim heading x')
 axs[6].legend()
 
-axs[7].plot(real_time_steps, real_heading[:, 1], label='real heading y')
-axs[7].plot(time_steps, heading[:, 1], label='sim heading y')
+axs[7].plot(real_time_steps[:show_len], real_heading[:show_len, 1], label='real heading y')
+axs[7].plot(time_steps[:show_len], heading[:show_len, 1], label='sim heading y')
 axs[7].legend()
 
-axs[8].plot(real_time_steps, real_heading[:, 2], label='real heading z')
-axs[8].plot(time_steps, heading[:, 2], label='sim heading z')
+axs[8].plot(real_time_steps[:show_len], real_heading[:show_len, 2], label='real heading z')
+axs[8].plot(time_steps[:show_len], heading[:show_len, 2], label='sim heading z')
 axs[8].legend()
 
 # lateral
-axs[9].plot(real_time_steps, real_lateral[:, 0], label='real lateral x')
-axs[9].plot(time_steps, lateral[:, 0], label='sim lateral x')
+axs[9].plot(real_time_steps[:show_len], real_lateral[:show_len, 0], label='real lateral x')
+axs[9].plot(time_steps[:show_len], lateral[:show_len, 0], label='sim lateral x')
 axs[9].legend()
 
-axs[10].plot(real_time_steps, real_lateral[:, 1], label='real lateral y')
-axs[10].plot(time_steps, lateral[:, 1], label='sim lateral y')
+axs[10].plot(real_time_steps[:show_len], real_lateral[:show_len, 1], label='real lateral y')
+axs[10].plot(time_steps[:show_len], lateral[:show_len, 1], label='sim lateral y')
 axs[10].legend()
 
-axs[11].plot(real_time_steps, real_lateral[:, 2], label='real lateral z')
-axs[11].plot(time_steps, lateral[:, 2], label='sim lateral z')
+axs[11].plot(real_time_steps[:show_len], real_lateral[:show_len, 2], label='real lateral z')
+axs[11].plot(time_steps[:show_len], lateral[:show_len, 2], label='sim lateral z')
 axs[11].legend()
 
 # up
-axs[12].plot(real_time_steps, real_up[:, 0], label='real up x')
-axs[12].plot(time_steps, up[:, 0], label='sim up x')
+axs[12].plot(real_time_steps[:show_len], real_up[:show_len, 0], label='real up x')
+axs[12].plot(time_steps[:show_len], up[:show_len, 0], label='sim up x')
 axs[12].legend()
 
-axs[13].plot(real_time_steps, real_up[:, 1], label='real up y')
-axs[13].plot(time_steps, up[:, 1], label='sim up y')
+axs[13].plot(real_time_steps[:show_len], real_up[:show_len, 1], label='real up y')
+axs[13].plot(time_steps[:show_len], up[:show_len, 1], label='sim up y')
 axs[13].legend()
 
-axs[14].plot(real_time_steps, real_up[:, 2], label='real up z')
-axs[14].plot(time_steps, up[:, 2], label='sim up z')
+axs[14].plot(real_time_steps[:show_len], real_up[:show_len, 2], label='real up z')
+axs[14].plot(time_steps[:show_len], up[:show_len, 2], label='sim up z')
 axs[14].legend()
 
 plt.tight_layout()
--- real2sim/plot_following.py
+++ real2sim/plot_following.py
@@ -3,12 +3,12 @@ import matplotlib.pyplot as plt
 import torch
 import pandas as pd
 
-plot_sim = True
+plot_sim = False
 
 start_T = 0
 # sim data
-sim_target = torch.load('/home/jiayu/OmniDrones/real2sim/DATT/smooth5_scale2_5/0_25T_br_0_5pi/sim_action.pt')
-sim_real = torch.load('/home/jiayu/OmniDrones/real2sim/DATT/smooth5_scale2_5/0_25T_br_0_5pi/sim_rpy.pt')
+sim_target = torch.load('/home/jiayu/OmniDrones/real2sim/DATT/scale2_5_smooth5/sim_action.pt')
+sim_real = torch.load('/home/jiayu/OmniDrones/real2sim/DATT/scale2_5_smooth5/sim_rpy.pt')
 sim_target_rpy = torch.stack(sim_target)[:, 0, :3].to('cpu').numpy()[start_T:] * np.pi
 sim_target_thrust = torch.stack(sim_target)[:, 0, 3].to('cpu')[start_T:]
 sim_target_thrust = torch.clamp((sim_target_thrust + 1) / 2, min = 0.0, max = 0.9).numpy() * 2**16
@@ -17,14 +17,19 @@ time_steps = np.arange(len(sim_real_rpy))
 
 # real data: load from rosbag        
 start_T_real = 500
-end_T_real = 500 + 1500
-df = pd.read_csv('/home/jiayu/OmniDrones/real2sim/PID/cf14_figure8.csv', skip_blank_lines=True)
+end_T_real = 500 + 180
+df = pd.read_csv('/home/jiayu/OmniDrones/real2sim/DATT/scale2_5_smooth5/cf14_fast.csv', skip_blank_lines=True)
 preprocess_df = df[(df[['target_rate.thrust']].to_numpy()[:,0] > 0)][start_T_real:end_T_real]
 real_target = preprocess_df[['target_rate.r', 'target_rate.p', 'target_rate.y']].to_numpy() * np.pi / 180
 real_real_rpy = preprocess_df[['real_rate.r', 'real_rate.p', 'real_rate.y']].to_numpy()
+real_thrust = preprocess_df['real_rate.thrust']
+target_thrust = preprocess_df['target_rate.thrust']
 real_time_steps = np.arange(len(real_real_rpy))
 
-fig, axs = plt.subplots(3, 1, figsize=(12, 8))
+if plot_sim:
+    fig, axs = plt.subplots(3, 1, figsize=(12, 8))
+else:
+    fig, axs = plt.subplots(4, 1, figsize=(12, 8))
 
 # # sim roll, pitch, yaw
 # axs[0,0].plot(time_steps, sim_real_rpy[:, 0], label='sim roll rate')
@@ -65,6 +70,10 @@ else:
     axs[2].plot(real_time_steps, real_target[:, 2], label='real target yaw rate')
     axs[2].plot(real_time_steps, -real_real_rpy[:, 2], label='real yaw rate')
     axs[2].legend()
+
+    axs[3].plot(real_time_steps, target_thrust, label='real target thrust')
+    axs[3].plot(real_time_steps, real_thrust, label='real thrust')
+    axs[3].legend()
 
 plt.tight_layout()
 plt.savefig('ctbr_following')
