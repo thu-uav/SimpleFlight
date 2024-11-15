8ecbaac05ad922d5afaca8e9197a847f5fb87a9a
jiayu-ch15 <jiayu-ch15@mails.tsinghua.edu.cn>
2024-11-15 21:21:10 +0800

    update

--- real2sim/plot_following.py
+++ real2sim/plot_following.py
@@ -3,14 +3,14 @@ import matplotlib.pyplot as plt
 import torch
 import pandas as pd
 
-plot_sim = False
+plot_sim = True
 
 start_T = 0
 min_thrust = 0.0
 max_thrust = 0.9
 # sim data
-sim_target = torch.load('/home/jiayu/OmniDrones/real2sim/fast/0_0to0_8/sim_action.pt')
-sim_real = torch.load('/home/jiayu/OmniDrones/real2sim/fast/0_0to0_8/sim_rpy.pt')
+sim_target = torch.load('/home/jiayu/OmniDrones/real2sim/fast/reward/wo_up/sim_action.pt')
+sim_real = torch.load('/home/jiayu/OmniDrones/real2sim/fast/reward/wo_up/sim_rpy.pt')
 sim_target_rpy = torch.stack(sim_target)[:, 0, :3].to('cpu').numpy()[start_T:] * np.pi
 sim_target_thrust = torch.clamp((torch.stack(sim_target)[:, 0, 3].to('cpu')[start_T:] + 1.0) / 2.0, min=min_thrust, max=max_thrust)
 # thrust: 0.6328 # init for hover
@@ -19,16 +19,17 @@ sim_target_thrust = sim_target_thrust.numpy()
 sim_real_rpy = torch.stack(sim_real)[:-1, 0, :3].to('cpu').numpy()[start_T:]
 time_steps = np.arange(len(sim_target_thrust))
 
-# real data: load from rosbag        
-start_T_real = 500
-end_T_real = 500 + 400
-df = pd.read_csv('/home/jiayu/OmniDrones/real2sim/cf14_slow.csv', skip_blank_lines=True)
-preprocess_df = df[(df[['target_rate.thrust']].to_numpy()[:,0] > 0)][start_T_real:end_T_real]
-real_target = preprocess_df[['target_rate.r', 'target_rate.p', 'target_rate.y']].to_numpy() * np.pi / 180
-real_real_rpy = preprocess_df[['real_rate.r', 'real_rate.p', 'real_rate.y']].to_numpy()
-real_thrust = preprocess_df['real_rate.thrust'] / 2**16
-target_thrust = preprocess_df['target_rate.thrust'] / 2**16
-real_time_steps = np.arange(len(real_real_rpy))
+# real data: load from rosbag   
+if not plot_sim:     
+    start_T_real = 500
+    end_T_real = 500 + 400
+    df = pd.read_csv('/home/jiayu/OmniDrones/real2sim/cf14_slow.csv', skip_blank_lines=True)
+    preprocess_df = df[(df[['target_rate.thrust']].to_numpy()[:,0] > 0)][start_T_real:end_T_real]
+    real_target = preprocess_df[['target_rate.r', 'target_rate.p', 'target_rate.y']].to_numpy() * np.pi / 180
+    real_real_rpy = preprocess_df[['real_rate.r', 'real_rate.p', 'real_rate.y']].to_numpy()
+    real_thrust = preprocess_df['real_rate.thrust'] / 2**16
+    target_thrust = preprocess_df['target_rate.thrust'] / 2**16
+    real_time_steps = np.arange(len(real_real_rpy))
 
 fig, axs = plt.subplots(4, 1, figsize=(12, 8))
 
