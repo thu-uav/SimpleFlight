3b0dfc3333f906b18a736842587aa054baf94d74
jiayu-ch15 <jiayu-ch15@mails.tsinghua.edu.cn>
2025-03-17 11:03:42 +0800

    add sac

--- omni_drones/learning/sac.py
+++ omni_drones/learning/sac.py
@@ -62,10 +62,10 @@ class SACPolicy(object):
 
         # self.obs_name = f"{self.agent_spec.name}.obs"
         # self.act_name = ("action", f"{self.agent_spec.name}.action")
-        # if agent_spec.state_spec is not None:
-        #     self.state_name = f"{self.agent_spec.name}.state"
-        # else:
-        #     self.state_name = f"{self.agent_spec.name}.obs"
+        if agent_spec.state_spec is not None:
+            self.state_name = f"{self.agent_spec.name}.state"
+        else:
+            self.state_name = f"{self.agent_spec.name}.obs"
         # self.reward_name = f"{self.agent_spec.name}.reward"
         self.obs_name = ("agents", "observation")
         self.act_name = ("agents", "action")
